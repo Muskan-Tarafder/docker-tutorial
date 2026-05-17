aea766c0720fa2fe772f2c647a7df6ffa79d6586
Muskan Tarafder <muskantarafder357@gmail.com>
2026-05-17 21:52:46 +0530

    Hardcode port 8000 to match Dockerfile EXPOSE directive

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,4 @@
 
 python manage.py makemigrations
 python manage.py migrate
-PORT="${PORT:-8000}"
-gunicorn _core.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn _core.wsgi:application --bind 0.0.0.0:8000
